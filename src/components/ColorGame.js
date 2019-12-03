68434af77ee82d4d59e0e2a56a3203e9670508bf
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2019-12-03 21:49:50 +0530

    base created

--- src/components/ColorGame.js
+++ src/components/ColorGame.js
@@ -1,12 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import MenuBar from "./MenuBar";
+import CardContainer from "./CardContainer";
+import Card from "./Card";
 
-function ColorGame(props) {
+function ColorGame() {
+  const [totalColor, setTotalColor] = useState(6);
+  const [colors, setColors] = useState([]);
+  const [rightAnswer, setAnswer] = useState("");
+  const [isWon, setStatus] = useState(false);
+  const [isStarted, setStart] = useState(false);
+
+  useEffect(() => {
+    generateRandomColor();
+    //return () => generateRandomColor();
+  }, [totalColor]);
+
+  const bindCards = data => {
+    return data.map((item, index) => {
+      return (
+        <Card key={index} cardClick={handleCardClick} backgroundColor={item} />
+      );
+    });
+  };
+
+  const handleCardClick = data => {
+    console.log(data, rightAnswer, data === rightAnswer);
+    if (!isStarted) {
+      setStart(true);
+    }
+    if (data === rightAnswer) {
+    } else {
+      // const arr = colors;
+      // arr.indexOf(data) = "rgb(35, 35, 35)";
+      // setColors([
+      //   ...colors,
+      //   (colors[colors.indexOf(data)] = "rgb(35, 35, 35)")
+      // ]);
+    }
+  };
+
+  const handleResetClick = () => {
+    console.log("reset");
+  };
+  const handleModeClick = data => {
+    // console.log(data);
+    if (data === "easy") {
+      setTotalColor(3);
+    } else {
+      setTotalColor(6);
+    }
+    // setTotalColor()
+  };
+
+  const randomColor = () => {
+    var r = Math.floor(Math.random() * 256);
+
+    var g = Math.floor(Math.random() * 256);
+
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+  };
+
+  const generateRandomColor = () => {
+    var arr = [];
+    for (var i = 0; i < totalColor; i++) {
+      arr.push(randomColor());
+    }
+    setColors(arr);
+    pickColor(arr);
+  };
+
+  const pickColor = arr => {
+    var random = Math.floor(Math.random() * arr.length);
+    setAnswer(arr[random]);
+  };
+  console.log(colors);
   return (
     <>
-      <Header colorName="rgb(135, 197, 103)" />
-      <MenuBar />
+      <Header colorName={rightAnswer} />
+      <MenuBar
+        handleReset={handleResetClick}
+        handleModeClick={handleModeClick}
+        isHard={totalColor === 6 ? true : false}
+        isWon={isWon}
+        isStarted={isStarted}
+      />
+      <CardContainer>{bindCards(colors)}</CardContainer>
     </>
   );
 }
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -13,7 +13,7 @@ const defaultProps = {
   isStarted: false
 };
 function MenuBar(props) {
-  const { isHard, isWon, isStarted } = props;
+  const { isHard, isWon, isStarted, handleReset, handleModeClick } = props;
   const easyModeClassName = ClassNames("btn", {
     selected: !isHard
   });
@@ -22,13 +22,31 @@ function MenuBar(props) {
   });
   const messageText = isStarted ? (isWon ? "Correct!" : "try again!") : "";
   const startText = isWon ? "Play Again?" : "New Colors";
+
+  const handleMode = event => {
+    handleModeClick(event.target.getAttribute("data-mode"));
+  };
   return (
     <div className="menu-bar__wrapper">
       <div className="menu-bar">
-        <button className="btn">{startText}</button>
+        <button onClick={handleReset} className="btn">
+          {startText}
+        </button>
         <span className="message">{messageText}</span>
-        <button className={easyModeClassName}>Easy</button>
-        <button className={hardModeClassName}>Hard</button>
+        <button
+          onClick={handleMode}
+          data-mode="easy"
+          className={easyModeClassName}
+        >
+          Easy
+        </button>
+        <button
+          onClick={handleMode}
+          data-mode="hard"
+          className={hardModeClassName}
+        >
+          Hard
+        </button>
       </div>
     </div>
   );
