5537ae31d3ef06ccd044b578e28d019d20d056b2
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2019-12-14 22:44:03 +0530

    Updates

---
+++ precache-manifest.cfe706ac24ced3fc6db386b32c7dff6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c278f0476bddfea874deb4d0468075df",
+    "url": "/guess-the-color/index.html"
+  },
+  {
+    "revision": "1ce103c20f1acbb3b180",
+    "url": "/guess-the-color/static/css/main.0ad4edd3.chunk.css"
+  },
+  {
+    "revision": "a38a4bc972af0e22b554",
+    "url": "/guess-the-color/static/js/2.d45b99d2.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/guess-the-color/static/js/2.d45b99d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ce103c20f1acbb3b180",
+    "url": "/guess-the-color/static/js/main.91fd8557.chunk.js"
+  },
+  {
+    "revision": "7593e6f7484fcaf07db5",
+    "url": "/guess-the-color/static/js/runtime-main.0fc5d9a3.js"
+  }
+]);
